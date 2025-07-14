30e763b5c53274e154038e8345a6a40fff3707e6
csantv <csantve@gmail.com>
2025-07-14 17:23:59 -0500

    process pdf corpus

--- embedding.py
+++ embedding.py
@@ -1,31 +1,28 @@
-import bs4
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_pymupdf4llm import PyMuPDF4LLMLoader
+from langchain_text_splitters import CharacterTextSplitter
 
 embeddings = HuggingFaceEmbeddings(model_name="Qwen/Qwen3-Embedding-0.6B")
+
+file_paths = [
+    "./corpus/CORPUS CARLOS.pdf"
+]
+
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+all_splits = []
+
+for file_path in file_paths:
+    loader = PyMuPDF4LLMLoader(file_path, mode="single")
+    docs = loader.load()
+    doc_splits = text_splitter.split_documents(docs)
+    all_splits.extend(doc_splits)
 
 vector_store = Chroma(
     collection_name="example_collection",
     embedding_function=embeddings,
     persist_directory="./chroma_langchain_db",  # Where to save data locally, remove if not necessary
 )
-
-# Load and chunk contents of the blog
-loader = WebBaseLoader(
-    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
-    bs_kwargs=dict(
-        parse_only=bs4.SoupStrainer(
-            class_=("post-content", "post-title", "post-header")
-        )
-    ),
-)
-docs = loader.load()
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-
-all_splits = text_splitter.split_documents(docs)
 
 # Index chunks
 _ = vector_store.add_documents(documents=all_splits)
