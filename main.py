a1acfa549ca2096c8f7b4099b5f62dd38c588ad8
csantv <csantve@gmail.com>
2025-07-16 15:11:20 -0500

    updated some stuff

--- main.py
+++ main.py
@@ -7,35 +7,38 @@ from langchain_ollama import OllamaLLM
 
 llm = OllamaLLM(
     model="qwen3:0.6b",
-    temperature=0.3
+    temperature=0.5
 )
-
 embeddings = HuggingFaceEmbeddings(model_name="Qwen/Qwen3-Embedding-0.6B")
 
 vector_store = Chroma(
     embedding_function=embeddings,
     persist_directory="./ec2_chroma_vectorstore", 
 )
-
-prompt = ChatPromptTemplate.from_template("""
-Answer in spanish briefly
-
-{context}
-
-Question: {input}
-Answer: /no_think""")
-
 retriever = vector_store.as_retriever(
     search_type="similarity",
     search_kwargs={"k": 3}
 )
 
-combine_docs_chain = create_stuff_documents_chain(llm, prompt)
+prompt = """
+Answer in spanish briefly
+
+{context}
+
+Question: {input}
+Answer: /no_think"""
+
+prompt_template = ChatPromptTemplate.from_template(prompt)
+combine_docs_chain = create_stuff_documents_chain(llm, prompt_template)
 retrieval_chain = create_retrieval_chain(retriever, combine_docs_chain)
 
 # "Cómo puedo hacer que mi backend y frontend se comuniquen"
 while True:
     query = input(">Ingrese consulta: ")
+
+    if not query:
+        continue
+
     if query == "EXIT":
         break
 
