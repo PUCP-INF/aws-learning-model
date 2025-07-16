dcd2ffea19cb3fb4a46ea0551d6e2691a8d9676a
yosepgx <46633004+yosepgx@users.noreply.github.com>
2025-07-16 10:50:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,8 @@ llm = OllamaLLM(
 embeddings = HuggingFaceEmbeddings(model_name="Qwen/Qwen3-Embedding-0.6B")
 
 vector_store = Chroma(
-    collection_name="example_collection",
     embedding_function=embeddings,
-    persist_directory="./chroma_langchain_db",  # Where to save data locally, remove if not necessary
+    persist_directory="./ec2_chroma_vectorstore", 
 )
 
 prompt = ChatPromptTemplate.from_template("""
